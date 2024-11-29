66ad87c156f5e44813352dc1ac9824bbaa915888
Wayne Maurer <2899448+wmaurer@users.noreply.github.com>
2024-11-29 20:39:23 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 import { NgScrollbarModule } from 'ngx-scrollbar';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, NgScrollbarModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [CommonModule, NgScrollbarModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ngx-scrollbar-bug';
